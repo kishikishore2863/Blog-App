6b36928c5b4815312dc8edebcb95b42dc8cc7c9c
kishikishore2863 <kishikishore.2863@gmail.com>
2024-09-19 20:28:03 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Header from "./component/Header";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from "flowbite-react";
+
+const Home = () => {
+  return (<>
+    <div>Home</div>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default Home
+
+
