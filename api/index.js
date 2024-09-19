7119ea90558f2ced468cb20e37632f3300da38e1
kishikishore2863 <kishikishore.2863@gmail.com>
2024-09-19 20:28:40 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB).then(
+    console.log('mongoDb is connected')
+)
+
+const app =express()
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+});
